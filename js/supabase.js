1e0ff2cc7a5344d6502d180022d676eef85cf3d7
Submarine-boy <wavyminer4@gmail.com>
2026-08-18 17:42:35 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://qljnonlhccxcevrwzykp.supabase.co";
+const SUPABASE_URL = "https://xrxxdwhjcacjfjimvjyc.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_2Y7h0O3-41EEehAXj6kL5g_yCFPYiYR";
+    "sb_publishable_R689TV9oJEeoB2cKzuvEjg_Ok4Wuutw";
 
 export const supabase = createClient(
     SUPABASE_URL,
